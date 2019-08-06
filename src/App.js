56a53200166ecc2e5414231556a9f20bf9c951f3
Spencer-1 <jonathanmspencer@gmail.com>
2019-08-06 23:25:31 +0100

    end of section 5 - includes css modules to dynamically select styling

--- src/App.js
+++ src/App.js
@@ -48,16 +48,9 @@ class App extends Component {
   /// This is where we render everything!!! wooooaaah! :D
 
   render() {
-    const style ={
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid black',
-      padding: '8px',
-      cusor: 'pointer',
-    };
 
     let persons = null;
+    let btnClass = '';
 
     if ( this.state.showPersons ) {
       persons = (
@@ -72,8 +65,8 @@ class App extends Component {
           })}
         </div>
       );
-      // conditionally applying styling
-      style.backgroundColor = 'red';
+
+      btnClass = classes.Red;
     }
 
     const assignedClasses = [];
@@ -91,7 +84,7 @@ class App extends Component {
         <h1>Hi, I'm a React App</h1>
         <p className={assignedClasses.join(' ')}>This is really working!</p>
         <button
-          style={style}
+        className = {btnClass}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
